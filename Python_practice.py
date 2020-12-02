8cd4ea0d747dd5c90e053192e0285bd662902f6c
Srinivasan Raajagopal <74391103+raajasrini@users.noreply.github.com>
2020-12-02 00:02:37 -0800

    Create Python_practice.py

---
+++ Python_practice.py
@@ -0,0 +1,6 @@
+print("Hello World !")
+voting_data=[]
+voting_data.append({"county":"Arapahoe","registered_voters":"422829"})
+voting_data.append({"county":"Denver","registered_voters":"463353"})
+voting_data.append({"county":"Jefferson","registered_voters":"432438"})
+print(voting_data)
